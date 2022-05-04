418920b2ae8ea4d327a2667e34a9d4836c9e877a
Faris <farisspahic03@gmail.com>
2022-05-04 09:36:39 +0200

    Alpha version of project has been built. All working but with couple of major defficiencies. Must fix speech mechanism and format the output text. Also, should add topP to request...

--- app/src/main/java/com/example/textgeneratorfinalversion/MainActivity.java
+++ app/src/main/java/com/example/textgeneratorfinalversion/MainActivity.java
@@ -13,9 +13,15 @@ import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Objects;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -41,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         tuneBtn = findViewById(R.id.settingsButton);
         generatedTV = findViewById(R.id.resultTextView);
         promptET = findViewById(R.id.promptEditText);
+
+        SettingsActivity.tempVal = new Float(0.8);
+        SettingsActivity.topKVal = 999;
 
         readBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,27 +78,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 promptStr = promptET.getText().toString();
-
                 OkHttpClient client = new OkHttpClient();
 
                 MediaType mediaType = MediaType.parse("application/json");
-                String value = "{\r\"prompt\": \"In a shocking finding, scientist discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.\",\r\"temperature\": 0.8\r }";
-                @androidx.annotation.NonNull
-                RequestBody body = RequestBody.create(mediaType, value);
-            Request request = new Request.Builder()
-                    .url("https://gpt-text-generation.p.rapidapi.com/completions?temperature=1&stream=false&top_k=999&seed=0")
-                    .post(body)
-                    .addHeader("content-type", "application/json")
-                    .addHeader("X-RapidAPI-Host", "gpt-text-generation.p.rapidapi.com")
-                    .addHeader("X-RapidAPI-Key", "946b1f6709msh02a21458e3673fdp1f72c2jsn1f79547e501d")
-                    .build();
+                String value = "{\"prompt\": \""+ promptStr +"\",\"temperature\": "+SettingsActivity.tempVal+", \"top_k\": " + SettingsActivity.topKVal + " }";
+                System.out.println("Passu " + value);
 
-                try {
-                    Response response = client.newCall(request).execute();
-                    generatedTV.setText(response.body().string());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                RequestBody body = RequestBody.create(mediaType, value);
+                Request request = new Request.Builder()
+                        .url("https://api.textsynth.com/v1/engines/gptj_6B/completions")
+                        .post(body)
+                        .addHeader("Content-Type", "application/json")
+                        .addHeader("Authorization", "Bearer 70f14c8ecbdc3a2a160c672ba96eb1b9")
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                   @Override
+                   public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                       e.printStackTrace();
+                   }
+
+                   @Override
+                   public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                       if(response.isSuccessful()) {
+                        String txt = response.body().string();
+
+                              // JSONObject json = new JSONObject(Objects.requireNonNull(response.body()).string());
+                               //System.out.println(json);
+
+                           MainActivity.this.runOnUiThread(new Runnable() {
+                               @Override
+                               public void run() {
+                                       generatedTV.setText(txt);
+
+                               }
+                           });
+                       }
+                   }
+               });
 
             }
         });
--- app/src/main/java/com/example/textgeneratorfinalversion/SettingsActivity.java
+++ app/src/main/java/com/example/textgeneratorfinalversion/SettingsActivity.java
@@ -1,14 +1,39 @@
 package com.example.textgeneratorfinalversion;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.slider.RangeSlider;
+
 public class SettingsActivity extends AppCompatActivity {
+    public static RangeSlider temperatureSlider, topKSlider;
+    public static Float tempVal;
+    public static Integer topKVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        temperatureSlider = findViewById(R.id.temperature);
+        topKSlider = findViewById(R.id.topk);
+
+        temperatureSlider.addOnChangeListener(new RangeSlider.OnChangeListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onValueChange(@NonNull RangeSlider slider, float value, boolean fromUser) {
+                tempVal = value;
+            }
+        });
+
+        topKSlider.addOnChangeListener(new RangeSlider.OnChangeListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onValueChange(@NonNull RangeSlider slider, float value, boolean fromUser) {
+                topKVal = Math.round(value);
+            }
+        });
     }
 }
